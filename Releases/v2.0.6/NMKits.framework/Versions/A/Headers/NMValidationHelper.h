bb96ba7d7c6af974b3d1914da3e7f2dca27f8843
Nikmesoft Co. Ltd <nikmesoft@gmail.com>
2013-10-09 17:10:59 +0700

    V2.0.6
    Fixed Google Plus function

---
+++ Releases/v2.0.6/NMKits.framework/Versions/A/Headers/NMValidationHelper.h
@@ -0,0 +1,15 @@
+//
+//  NMValidationHelper.h
+//  NMKits
+//
+//  Created by Linh NGUYEN on 9/14/13.
+//  Copyright (c) 2013 Nikmesoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NMValidationHelper : NSObject
+
++ (BOOL)validateEmail:(NSString *)email;
+
+@end
